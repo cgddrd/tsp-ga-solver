32c9f1eed67941470f2fe675ba49338dde5efdc1
Connor Goddard <hello@connorlukegoddard.com>
2015-11-04 22:26:27 +0000

    Minor changes.

--- tsp-generator/src/main/java/uk/ac/aber/clg11/tsp/TSPGenerator.java
+++ tsp-generator/src/main/java/uk/ac/aber/clg11/tsp/TSPGenerator.java
@@ -39,10 +39,10 @@ public class TSPGenerator {
 			CommandLine line = parser.parse(options, args);
 
 			if (line.hasOption("help") || !line.hasOption("points")) {
-				
+
 				HelpFormatter formatter = new HelpFormatter();
 				formatter.printHelp("TSEGenerator", options);
-				
+
 			} else {
 
 				int noOfPoints = Integer.parseInt(line.getOptionValue("points"));
@@ -81,52 +81,6 @@ public class TSPGenerator {
 			System.err.println("Error occured while running TSP generator.");
 			ex.printStackTrace();
 		}
-
-		/*
-		 * if (args.length < 1) {
-		 * 
-		 * System.out.println(
-		 * "Error: User must supply number of TSP coordinates to generate. Exiting application."
-		 * );
-		 * 
-		 * } else {
-		 * 
-		 * try {
-		 * 
-		 * int noOfPoints = Integer.parseInt(args[0]);
-		 * 
-		 * // Object.equals() also checks for nulls at the same time. String
-		 * fileName = (args.length >= 2) && (args[1] != null) &&
-		 * (!args[1].equals("null")) ? args[1] : "." + File.separator +
-		 * "TSPCoordsExport.csv";
-		 * 
-		 * int gridSize = (args.length >= 3) && (args[2] != null) &&
-		 * (Integer.parseInt(args[2]) > 0) ? Integer.parseInt(args[2]) : 200;
-		 * 
-		 * TSPCoord[] tspCoords = generateTSPPoints(noOfPoints, gridSize);
-		 * 
-		 * if (tspCoords != null) {
-		 * 
-		 * if (writeCSVFile(fileName, tspCoords)) { System.out.println(
-		 * "Success: TSP coordinates generated successfully."); } else {
-		 * System.out.println(
-		 * "Error: Unable to write CSV file. Are you sure writer permissions are available?"
-		 * ); }
-		 * 
-		 * } else {
-		 * 
-		 * System.out.println("Error: No coordinates to write to file.");
-		 * 
-		 * }
-		 * 
-		 * } catch (NumberFormatException nFE) { System.out.println(
-		 * "Error: First argument for number of TSP coordinates must be a number."
-		 * ); } catch (Exception ex) { System.out.println(
-		 * "Error occured while running TSP generator."); ex.printStackTrace();
-		 * } }
-		 * 
-		 * System.out.println("Application exited.");
-		 */
 
 	}
 
